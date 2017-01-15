e7affddeadf5adbe42f8ee28e21fb4c2296fbfeb
hotehrud <root@devgumi.cafe24.com>
2017-01-15 19:35:45 +0900

    환경 분리 초기 셋팅

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const env = process.env.NODE_ENV || "development";
-const config = require('./config/config.json')[env];
+const config = require('./config/environments');
 const db = require('./db');
 
 const app = express();
