3468db60725ade8d55bae25ce64967f53a146eb3
EC2 Default User <ec2-user@ip-172-31-28-6.ap-northeast-2.compute.internal>
2017-02-22 03:12:29 +0000

    검색 API URL 정보 파일 분리

--- app/api/search/search.properties.js
+++ app/api/search/search.properties.js
@@ -7,17 +7,25 @@ const apiProperties = {
         searchAdult: "https://openapi.naver.com/v1/search/adult?query=",
         searchEncyc: "https://openapi.naver.com/v1/search/encyc?query=",
         searchMovie: "https://openapi.naver.com/v1/search/movie?query=",
-        searchCafearticle: "https://openapi.naver.com/v1/search/cafearticle?query=",
+        searchCafe: "https://openapi.naver.com/v1/search/cafearticle?query=",
         searchKin: "https://openapi.naver.com/v1/search/kin?query=",
         searchLocal: "https://openapi.naver.com/v1/search/local?query=",
         searchErrata: "https://openapi.naver.com/v1/search/errata?query=",
-        searchWebkr: "https://openapi.naver.com/v1/search/webkr?query=",
+        searchWeb: "https://openapi.naver.com/v1/search/webkr?query=",
         searchImage: "https://openapi.naver.com/v1/search/image?query=",
         searchShop: "https://openapi.naver.com/v1/search/shop?query=",
         searchDoc: "https://openapi.naver.com/v1/search/doc?query="
     },
     daum: {
-
+        apiKey: "a5b0eef3c46e86443e3b73624f11488a",
+        searchBoard: "https://apis.daum.net/search/board?apikey=a5b0eef3c46e86443e3b73624f11488a",
+        searchVclip: "https://apis.daum.net/search/vclip?apikey=a5b0eef3c46e86443e3b73624f11488a",
+        searchBlog: "https://apis.daum.net/search/blog?apikey=a5b0eef3c46e86443e3b73624f11488a",
+        searchWeb: "https://apis.daum.net/search/web?apikey=a5b0eef3c46e86443e3b73624f11488a",
+        searchImage: "https://apis.daum.net/search/image?apikey=a5b0eef3c46e86443e3b73624f11488a",
+        searchKin: "https://apis.daum.net/search/knowledge?apikey=a5b0eef3c46e86443e3b73624f11488a",
+        searchBook: "https://apis.daum.net/search/book?apikey=a5b0eef3c46e86443e3b73624f11488a",
+        searchCafe: "https://apis.daum.net/search/cafe?apikey=a5b0eef3c46e86443e3b73624f11488a"
     }
 }
 
