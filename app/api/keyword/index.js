4ff69233bf25779a8400174434d19d725cf3c868
EC2 Default User <ec2-user@ip-172-31-28-6.ap-northeast-2.compute.internal>
2017-03-01 05:51:56 +0000

    All keyword search-api

--- app/api/keyword/index.js
+++ app/api/keyword/index.js
@@ -3,11 +3,13 @@ const router = express.Router();
 
 const controller = require('./keyword.controller');
 
-// All Keyword
 router.get('/', controller.index);
 
 // Specific Keyword
 router.get('/:keyword', controller.show);
+
+// All Keyword
+router.get('/list/:keyword', controller.list);
 
 // Keyword is store in redis. The reason is the number of keyword to check in real-time.
 router.post('/', controller.create);
--- app/api/keyword/keyword.controller.js
+++ app/api/keyword/keyword.controller.js
@@ -19,6 +19,20 @@ exports.show = (req, res) => {
         });
 }
 
+exports.list = (req, res) => {
+    let pageno = req.query.pageno ? req.query.pageno * 10 : 0;
+
+    models.Keyword.findAll({
+        offset: pageno,
+        limit: 10
+    })
+        .then(result => {
+
+            return result.length ? res.status(200).json(result) : res.status(200).json({message: 'empty'})
+
+        })
+}
+
 exports.create = (req, res) => {
     return res.status(200).json(req.body);
 }
