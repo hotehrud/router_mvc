72a7edffc7da7e70499922c3f9d3cdd13a8fc24a
EC2 Default User <ec2-user@ip-172-31-28-6.ap-northeast-2.compute.internal>
2017-03-13 03:54:21 +0000

    Additional function API initial commit

--- app/api/bookmark/bookmark.controller.js
+++ app/api/bookmark/bookmark.controller.js
@@ -1,29 +1,79 @@
-const bookmark = require('../../models')['Bookmark'];
+const Bookmark = require('../../models')['Bookmark'];
 
 exports.index = (req, res) => {
     res.status(200).json({'msg': 'Hello bookmark'});
 }
 
 exports.show = (req, res) => {
+    let id = req.params.id;
+    let user = req.user;
 
+    if (!user) {
+        return res.status(401).json({msg: 'You need login'});
+    }
+
+    Bookmark.findOne({
+        where: {
+            bookmark_id: id
+        }
+    })
+        .then(result => {
+
+            return result ? res.status(200).json(result) : res.status(200).json({message: 'empty'})
+
+        });
 }
 
 exports.list = (req, res) => {
+    let pageno = req.query.pageno ? req.query.pageno * 10 : 0;
+    let user = req.user;
 
+    if (!user) {
+        return res.status(401).json({msg: 'You need login'});
+    }
+
+    Bookmark.findAll({
+        offset: pageno,
+        limit: 10
+    })
+        .then(result => {
+
+            return result.length ? res.status(200).json(result) : res.status(200).json({message: 'empty'})
+
+        })
 }
 
 exports.destroy = (req, res) => {
+    let id = req.params.id;
+    let user = req.user;
 
+    if (!user) {
+        return res.status(401).json({msg: 'You need login'});
+    }
+
+    Bookmark.destroy({
+        where: {
+            bookmark_id: id,
+            bookmark_user: user
+        }
+    })
+        .then(bookmark => {
+            if (!bookmark) {
+                return res.status(404).json({error: 'No Bookmark'});
+            }
+            return res.status(204).send();
+        });
 }
 
 exports.create = (req, res) => {
     let id = req.body.search_seq;
+    let user = req.user;
 
-    if (!req.user) {
+    if (!user) {
         return res.status(401).json({msg: 'You need login'});
     }
 
-    bookmark.findOne({
+    Bookmark.findOne({
         where: {
             bookmark_id: id
         }
@@ -31,9 +81,9 @@ exports.create = (req, res) => {
         .then(data => {
             if (!data) {
 
-                bookmark.create({
+                Bookmark.create({
                     bookmark_id: id,
-                    bookmark_user: req.user
+                    bookmark_user: user
                 })
                     .then(data => {
                         return res.status(200).json({msg: 'success', value: JSON.stringify(data)});
